825be6bd13368c79d1e9a91a89ccb1b5379414f7
bryce foster <bryce1337@gmail.com>
2018-05-18 13:09:28 -0500

    Updated projects to use the latest nuget packages and added resource class to handle strings.

---
+++ src/U2F.Core/Exceptions/Resources.cs
@@ -0,0 +1,8 @@
+﻿namespace U2F.Core.Exceptions
+{
+    public static class Resources
+    {
+        public const string InvalidArguments = "The arguments passed the were not valid.";
+        public const string SignatureError = "Error when verifying signature";
+    }
+}
